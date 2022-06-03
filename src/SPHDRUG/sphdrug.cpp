0004fa678540830e5dd3dacc144506656b4cd65f
Joan Clark-Nicolas <joan.clark88@gmail.com>
2022-06-03 18:49:54 +0100

    slight change in the trick I put in correct_derivatives() for then pinv() fails

--- src/SPHDRUG/sphdrug.cpp
+++ src/SPHDRUG/sphdrug.cpp
@@ -116,8 +116,6 @@ namespace PLMD
       vector<double> sum_rcrossP;
       //for when correction of derivatives fails
       unsigned ndxfails;
-      vector<double> cum_sum_d;
-      vector<double> cum_sum_t;
       vector<double> d_Sphdrug_dx_old;
       vector<double> d_Sphdrug_dy_old;
       vector<double> d_Sphdrug_dz_old;
@@ -328,8 +326,6 @@ This does not seem to be affected by the environment variable $PLUMED_NUM_THREAD
         P = arma::vec(ncols);
         sum_P = vector<double>(3, 0);
         sum_rcrossP = vector<double>(3, 0);
-        cum_sum_d=vector<double>(3,0);
-        cum_sum_t=vector<double>(3,0);
         d_Sphdrug_dx_old = vector<double>(n_atoms, 0);
         d_Sphdrug_dy_old = vector<double>(n_atoms, 0);
         d_Sphdrug_dz_old = vector<double>(n_atoms, 0);
@@ -441,18 +437,17 @@ This does not seem to be affected by the environment variable $PLUMED_NUM_THREAD
       catch(...)
       {
         ndxfails++;
-        cum_sum_d[0]+=sum_d_dx;
-        cum_sum_d[1]+=sum_d_dy;
-        cum_sum_d[2]+=sum_d_dz;
-        cum_sum_t[0]+=sum_t_dx;
-        cum_sum_t[1]+=sum_t_dy;
-        cum_sum_t[2]+=sum_t_dz;
         cout << "Correction of derivatives failed at step "<< step <<", this is the " << ndxfails << "th time."<< endl;
         //cout << "Step "<< step << " sum_d: " << sum_d_dx << " " << sum_d_dy << " " << sum_d_dz << " / sum_t: " << sum_t_dx << " " << sum_t_dy << " " << sum_t_dz << endl;
         //cout << "Cummulative: sum_d: " << cum_sum_d[0] << " "<< cum_sum_d[1] << " " << cum_sum_d[2] << " / sum_t: "<< cum_sum_t[0] << " "<< cum_sum_t[1] << " " << cum_sum_t[2] << endl;
-        d_Sphdrug_dx_old=d_Sphdrug_dx;
-        d_Sphdrug_dy_old=d_Sphdrug_dy;
-        d_Sphdrug_dz_old=d_Sphdrug_dz;
+        #pragma omp parallel for
+        for (unsigned j=0;j<n_atoms;j++)
+         {
+          d_Sphdrug_dx_old[j]=d_Sphdrug_dx[j];
+          d_Sphdrug_dy_old[j]=d_Sphdrug_dy[j];
+          d_Sphdrug_dz_old[j]=d_Sphdrug_dz[j];
+         }
+        
         return;
       }
       
