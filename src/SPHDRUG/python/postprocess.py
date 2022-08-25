92ecb695f68749edafe71c97291c5ed904d824db
Joan Clark Nicolas <joan.clark88@gmail.com>
2022-08-25 12:08:07 +0100

    printing protein without probes and also a snapshot with all probes with activity > activity_min (parameter)

--- src/SPHDRUG/python/postprocess.py
+++ src/SPHDRUG/python/postprocess.py
@@ -1,6 +1,7 @@
 import argparse
 import mdtraj
 import numpy as np
+import pandas as pd
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -8,8 +9,9 @@ def parse():
     parser.add_argument('-t','--input_traj', nargs="?", help="GMX trajectory in xtc or trr format",default="traj.xtc")
     parser.add_argument('-x','--input_xyz', nargs="?", help="protein file issued by plumed",default=None)
     parser.add_argument('-n','--nprobes', nargs="?", type=int, help="Number of probes",default=1)
-    parser.add_argument('-o','--output', nargs="?", help="Protein output in pdb format",default="protein")
+    parser.add_argument('-o','--output', nargs="?", help="Protein output in pdb format",default="protein_probes")
     parser.add_argument('-s','--subset', nargs="?", help="subset of atoms for when protein.xyz is not supplied (VMS style selection)",default="all")
+    parser.add_argument('-a','--actmin', nargs="?", type=float, help="Minimum activity to print in the aligned pdb",default=1)
 
     args = parser.parse_args()
     return args
@@ -46,7 +48,7 @@ def mktraj(xyz,id):
     chain=top.add_chain()
     resname="PRB" #so that we always have 3 digits
     residue=top.add_residue(resname,chain)
-    top.add_atom("P"+str(id).zfill(2),mdtraj.element.germanium,residue)
+    top.add_atom("P"+str(id).zfill(2),mdtraj.element.helium,residue)
     trj=mdtraj.Trajectory(xyz,top)
     return trj
 
@@ -54,6 +56,32 @@ def stack_traj(protein_traj,probes_trj):
     for probe in probes_trj:
         protein_traj=protein_traj.stack(probe)
     return protein_traj
+
+def get_activity(nprobes):
+    activity=pd.DataFrame()
+    for i in range(0,nprobes):
+        name="P"+str(i).zfill(2)
+        filename="probe-"+str(i)+"-stats.csv"
+        activity[name]=pd.read_csv(filename,sep=" ").activity
+    return activity
+
+def print_probes_pdb(probes_trj,activity,activity_min):
+    top=mdtraj.Topology()
+    chain=top.add_chain()
+    xyz=[]
+    bfactors=[]
+    for i in range(0,len(activity)):
+        for j in range(0,len(activity.columns)):
+            atomname=activity.keys()[j]
+            if activity[atomname][i]<activity_min:
+                continue
+            residue=top.add_residue("PRB",chain)
+            top.add_atom("P"+str(i).zfill(2),mdtraj.element.helium,residue)
+            xyz.append(probes_trj.xyz[i][j])
+            bfactors.append(activity[atomname][i])
+    trj=mdtraj.Trajectory(xyz,top)
+    filename="probes_actmin_"+str(activity_min)+".pdb"
+    trj.save_pdb(filename,bfactors=bfactors)
 
 if __name__=="__main__":
 
@@ -68,7 +96,9 @@ if __name__=="__main__":
        subset.xyz=xyz_prot
     else:
         atomlist=traj_obj.topology.select(args.subset)
-        subset=subset=traj_obj.atom_slice(atomlist)
+        subset=traj_obj.atom_slice(atomlist)
+
+    n_atoms=len(traj_obj.xyz[0])
 
     #process probes
     probes_trj=[]
@@ -87,3 +117,10 @@ if __name__=="__main__":
     newtraj[0].save_gro(args.output+".gro")
     newtraj.save_xtc(args.output+".xtc")
 
+    prot_traj=newtraj.atom_slice(newtraj.topology.select("not resname PRB"))
+    prot_traj.save_gro("protein.gro")
+    prot_traj.save_xtc("protein.xtc")
+    
+    probes_trj=newtraj.atom_slice(newtraj.topology.select("resname PRB"))
+    activity=get_activity(args.nprobes)
+    print_probes_pdb(probes_trj,activity,args.actmin)
