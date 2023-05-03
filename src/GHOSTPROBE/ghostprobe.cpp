01ab23f31b487c5df0fc6969881e9834c3fd766c
Joan Clark Nicolas <joan.clark88@gmail.com>
2023-05-03 23:02:34 +0800

    reversed to the version where memory is allocated at the beginning even if matrices need to be resized

--- src/GHOSTPROBE/ghostprobe.cpp
+++ src/GHOSTPROBE/ghostprobe.cpp
@@ -110,6 +110,12 @@ namespace PLMD
       double sum_t_dz;
 
       //for when correction of derivatives fails
+      arma::mat A;
+      arma::mat B;
+      arma::mat Bt;
+      arma::vec v;
+      arma::vec c;
+
       double err_tol=0.00000000001;
       bool dumpderivatives;
 
@@ -331,6 +337,11 @@ This does not seem to be affected by the environment variable $PLUMED_NUM_THREAD
         tz=vector<double>(n_atoms,0);
         dxnonull=vector<bool>(n_atoms,false);
         dxnonull_size=0;
+        A=arma::mat(6,n_atoms);
+        B=arma::mat(6,n_atoms);
+        Bt=arma::mat(n_atoms,6);
+        v=arma::vec(n_atoms);
+        c=arma::vec(n_atoms);
       }
       else
       {
@@ -387,10 +398,10 @@ This does not seem to be affected by the environment variable $PLUMED_NUM_THREAD
       
       //cout << "Generating matrices" << endl;
       
-      arma::vec v(dxnonull_size);
+      v=arma::vec(dxnonull_size);
       fill(v.begin(),v.end(),1);
 
-      arma::mat A(6,dxnonull_size);
+      A=arma::mat(6,dxnonull_size);
       unsigned k=0;
       for (unsigned j=0; j<n_atoms; j++)
       {
@@ -407,9 +418,9 @@ This does not seem to be affected by the environment variable $PLUMED_NUM_THREAD
       
       //cout << "Matrix ops" << endl;
       //Apply https://math.stackexchange.com/questions/4686718/how-to-solve-a-linear-system-with-more-variables-than-equations-with-constraints/4686826#4686826
-      arma::mat B=arma::null(A);
-      arma::mat Bt=arma::trans(B);
-      arma::vec c=(B*pinv(Bt*B)*Bt*v); //if matrix isn't invertible, pinv() will provide the best approximation
+      B=arma::null(A);
+      Bt=arma::trans(B);
+      c=(B*pinv(Bt*B)*Bt*v); //if matrix isn't invertible, pinv() will provide the best approximation
       
       //cout << "Assigning correction" << endl;
       k=0;
