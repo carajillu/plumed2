0674256fb08778ac66fbd3bdc739fffa7bf9f3d9
Joan Clark-Nicolas <joan.clark88@gmail.com>
2024-01-02 19:59:53 +0100

    some fixes in the derivatives analysis script

--- src/GHOSTPROBE/python/derivatives_analysis.py
+++ src/GHOSTPROBE/python/derivatives_analysis.py
@@ -12,24 +12,27 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def hist2d(x,y,x_str,y_str,nbins,log):
+def hist2d(x,y,x_str,y_str,nbins,figout):
     g = sns.JointGrid(x=x, y=y)
-    hexbin = g.ax_joint.hexbin(x, y, gridsize=nbins, cmap='plasma', bins='log', mincnt=1)
-
-    # Adding marginal histograms
-    sns.histplot(x, ax=g.ax_marg_x, kde=False)
-    sns.histplot(y, ax=g.ax_marg_y, kde=False, orientation='horizontal')
+    hexbin = g.ax_joint.hexbin(x, y, gridsize=nbins, bins="log", cmap='plasma', mincnt=1,vmin=1,vmax=55968)
 
     # Setting labels
     g.set_axis_labels(x_str, y_str)
+    g.ax_joint.set_xlim([-0.3, 0.3])
+    #g.ax_joint.set_ylim([-2, 2])
     plt.subplots_adjust(top=1.0)
     plt.xlabel(x_str,fontweight="bold")
     plt.ylabel(y_str,fontweight="bold")
     cbar = plt.colorbar(hexbin, ax=g.ax_joint, orientation='vertical', pad=0.1)
     cbar.set_label('log(count)')
-    plt.savefig("derivatives.png", dpi=300)
+    plt.savefig(figout, dpi=300)
+    # Get and print vmin and vmax values
+    vmin, vmax = hexbin.get_clim()
+    print("vmin:", vmin, "vmax:", vmax)
 
 if __name__=="__main__":
     args=parse_args()
     derivatives=pd.read_csv(args.derivatives,sep=" ")
-    hist2d(derivatives.dx,derivatives.correction,"dx","correction",args.nbins,False)
+    hist2d(derivatives.dx,derivatives.correction,"dx","correction",args.nbins,"correction_dx.png")
+    hist2d(derivatives.dy,derivatives.correction,"dy","correction",args.nbins,"correction_dy.png")
+    hist2d(derivatives.dz,derivatives.correction,"dz","correction",args.nbins,"correction_dz.png")
