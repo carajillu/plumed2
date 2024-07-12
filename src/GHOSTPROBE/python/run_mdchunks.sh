b82fc6e156383b58de65d853510bf612bc066c3f
Joan Clark-Nicolas <joan.clark88@gmail.com>
2024-07-12 08:32:02 +0100

    Added bash script to run md in chunks

---
+++ src/GHOSTPROBE/python/run_mdchunks.sh
@@ -0,0 +1,21 @@
+start=1
+end=20
+plumed_srcdir=/home/joan/github/plumed2_carajillu/
+backupdir=/home/joan/MD/GHOSTPROBE/TEM1/1JWP/backup
+rootdir=$(pwd)
+gmx=/home/joan/local/local/gromacs2023.3-plumed2.10-dev/bin/gmx_mpi
+mdpdir=/home/joan/MD/mdp
+topname=1JWP.top
+for i in $(seq $start $end); do
+    cp -r $backupdir prod_$i
+    cd prod_$i
+    if [ $i -gt 0 ]; then
+       rm prod.tpr
+       sed -i "s/NPROBES=16 PROBESTRIDE=25000/NPROBES=16 PROBESTRIDE=25000 RESTART_PROBES/g" plumed.dat
+       let a=i-1
+       python3 $plumed_srcdir/src/GHOSTPROBE/python/restart.py -d ../prod_$a -n 16 
+       $gmx grompp -f $mdpdir/continuation.mdp -c ../prod_$a/prod.gro -p $topname -o prod.tpr
+    fi
+    $gmx mdrun -v -ntomp 8 -pin on -deffnm prod -plumed plumed.dat -nsteps 25000000
+    cd $rootdir 	
+done
