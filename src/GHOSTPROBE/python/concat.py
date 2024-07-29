5faf88f2eaaa988a1132028a35eb2e1f70a50539
Joan Clark-Nicolas <joan.clark88@gmail.com>
2024-07-29 12:31:46 +0100

    Now aligning to reference structure (makes mdpocket easier)

--- src/GHOSTPROBE/python/concat.py
+++ src/GHOSTPROBE/python/concat.py
@@ -8,16 +8,19 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p','--top', nargs="?", help="Topology file for mdtraj",default="prod_0/prod.gro")
     parser.add_argument('-t','--xtc', nargs="?", help="Name of the RAW Gromacs trajectory files (must be the same in all directories)",default="prod.xtc")
-    parser.add_argument('-s','--ndir', nargs="?", help="Number of simulation directories",default=1)
     parser.add_argument('-a','--pattern', nargs="?", help="pattern matching the directory names. Simulation index (starting at 0) will be appended at the end.",default="prod_")
+    parser.add_argument('-r','--reference', nargs="?", help="Reference structure for alignement. If not supplied, will align to first frame.",default=None)
+    parser.add_argument('-rs','--ref_selection', nargs="?", help="subset of atoms to align to reference (VMD style selection)",default="backbone")
+    parser.add_argument('-os','--out_selection', nargs="?", help="",default="not water")
     parser.add_argument('-b', '--nsim', nargs="?", type=int, help="Number of simulations sto concatenate (MUST start at 0, be consecutive and follow a pattern)",default=1)
     parser.add_argument('-x','--input_xyz', nargs="?", help="protein file issued by plumed (must be the same in all directories)",default="protein.xyz")
     parser.add_argument('-n','--nprobes', nargs="?", type=int, help="Number of probes (must be the same in all directories)",default=1)
     args = parser.parse_args()
     return args
 
-def concat_xtc(topology,names,outfile):
+def concat_xtc(topology,names,ref_obj,align_sel,output_sel,outfile):
     traj=mdtraj.load(names[0],top=topology)
+    selection=get_selection(ref_obj,traj,align_sel)
     print(names[0],traj)
     for i in range(1,len(names)):
         try:
@@ -29,7 +32,11 @@ def concat_xtc(topology,names,outfile):
         except:
           print(f"Could not load {names[i]}. Skipping.")
           continue
+    traj.image_molecules(inplace=True)
+    traj.superpose(reference=ref_obj[0],atom_indices=selection,ref_atom_indices=selection)
+    traj=traj.atom_slice(traj.topology.select(output_sel))
     traj.save_xtc(outfile)
+    traj[0].save_gro(outfile.replace(".xtc",".gro"))
     print(traj)
     return
 
@@ -57,6 +64,18 @@ def concat_df(names,outfile):
             continue
     df.to_csv(outfile,sep=" ", index=False)
     return df
+
+def get_selection(ref_obj,trj_obj,sel_str):
+    selection=ref_obj.topology.select(sel_str)
+    selection_trj=trj_obj.topology.select(sel_str)
+    assert len(selection)==len(selection_trj), "Selections have different number of atoms"
+    for i in range(0,len(selection)):
+        ref_name=ref_obj.topology.atom(selection[i]).name
+        trj_name=trj_obj.topology.atom(selection_trj[i]).name
+        assert ref_name==trj_name, f"Selections have different atoms at position {selection[i]}: {ref_name} vs {trj_name}"
+    print("assert selection OK")
+    return selection
+
           
 
 if __name__=="__main__":
@@ -71,7 +90,11 @@ if __name__=="__main__":
 
     print(args.top)
     print(xtc_lst)
-    xtc=concat_xtc(topology=args.top,names=xtc_lst,outfile="concat/"+args.xtc)
+    if args.reference is not None:
+        ref_obj=mdtraj.load(args.reference)[0]
+    else:
+        ref_obj=mdtraj.load(xtc_lst[0],top=args.top)[0]
+    xtc=concat_xtc(topology=args.top,names=xtc_lst,ref_obj=ref_obj,align_sel=args.ref_selection,output_sel=args.out_selection,outfile="concat/"+args.xtc)
     
     
     protein_lst=[]
