1c32590f7b3c6a7d9af43a2452d2987ac47b4b77
Joan Clark-Nicolas <joan.clark88@gmail.com>
2024-08-26 13:31:41 +0100

    specifying which plumed.dat you wanna use

--- src/GHOSTPROBE/python/run_mdchunks.sh
+++ src/GHOSTPROBE/python/run_mdchunks.sh
@@ -6,16 +6,17 @@ rootdir=$(pwd)
 gmx=/home/joan/local/local/gromacs2023.3-plumed2.10-dev/bin/gmx_mpi
 mdpdir=/home/joan/MD/mdp
 topname=1JWP.top
+plumedat=$3
 for i in $(seq $start $end); do
     cp -r $backupdir prod_$i
     cd prod_$i
     if [ $i -gt 0 ]; then
        rm prod.tpr
-       sed -i "s/NPROBES=16 PROBESTRIDE=25000/NPROBES=16 PROBESTRIDE=25000 RESTART_PROBES/g" plumed.dat
+       sed -i "s/NPROBES=16 PROBESTRIDE=25000/NPROBES=16 PROBESTRIDE=25000 RESTART_PROBES/g" $plumedat
        let a=i-1
        python3 $plumed_srcdir/src/GHOSTPROBE/python/restart.py -d ../prod_$a -n 16 
        $gmx grompp -f $mdpdir/continuation.mdp -c ../prod_$a/prod.gro -p $topname -o prod.tpr
     fi
-    $gmx mdrun -v -ntomp 8 -pin on -deffnm prod -plumed plumed.dat -nsteps 25000000
+    $gmx mdrun -v -ntomp 8 -pin on -deffnm prod -plumed $plumedat -nsteps 25000000
     cd $rootdir 	
 done
